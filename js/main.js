db18dd6d43b969d58f8d1d50102be1bbbcb1fe84
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-01-26 12:51:14 +0300

    add yandex map

--- js/main.js
+++ js/main.js
@@ -26,3 +26,53 @@ phoneInputs.forEach((input) => {
     }
   });
 });
+
+// Функция ymaps.ready() будет вызвана, когда
+// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+ymaps.ready(init);
+function init() {
+  // Создание карты.
+  var map = new ymaps.Map("map", {
+    // Координаты центра карты.
+    // Порядок по умолчанию: «широта, долгота».
+    // Чтобы не определять координаты центра карты вручную,
+    // воспользуйтесь инструментом Определение координат.
+    center: [59.943543, 30.338928],
+    // Уровень масштабирования. Допустимые значения:
+    // от 0 (весь мир) до 19.
+    zoom: 17,
+  });
+
+  var myPlacemark = new ymaps.Placemark(
+    [59.943543, 30.338928],
+    {
+      balloonContent: `
+      <div class="balloon">
+        <div class="balloon__address">Наб. реки Фонтанки 10-15</div>
+        <div class="balloon_contacts">
+          <a href="tel:+78121234567">8 (812) 123-45-67</a>
+        </div>
+      </div>
+      `,
+    },
+    {
+      iconLayout: "default#image",
+      iconImageHref: "./img/map/location-pin.svg",
+      icon_imagesize: [40, 40],
+      iconImageOffset: [-20, -40],
+    }
+  );
+
+  map.controls.remove("geolocationControl");
+  map.controls.remove("searchControl");
+  map.controls.remove("trafficControl");
+  map.controls.remove("typeSelector");
+  // map.controls.remove("fullscreenControl");
+  // map.controls.remove("zoomControl");
+  map.controls.remove("rulerControl");
+  map.controls.remove("scrollZoom");
+
+  // Размещение геообъекта на карте.
+  map.geoObjects.add(myPlacemark);
+  myPlacemark.balloon.open();
+}
