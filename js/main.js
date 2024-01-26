9353cb185878f655ae9ce6f268f8c7bb016a5987
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-01-26 05:59:57 +0300

    add mask input phone number

--- js/main.js
+++ js/main.js
@@ -7,3 +7,22 @@ navBtn.onclick = function () {
   nav.classList.toggle("header__top-row--mobile");
   document.body.classList.toggle("no-scroll");
 };
+
+mask("[data-tel-input]");
+
+const phoneInputs = document.querySelectorAll("[data-tel-input]");
+
+phoneInputs.forEach((input) => {
+  console.log(input);
+  input.addEventListener("input", () => {
+    if (input.value === "+") {
+      input.value = "";
+    }
+  });
+
+  input.addEventListener("blur", () => {
+    if (input.value === "+") {
+      input.value = "";
+    }
+  });
+});
